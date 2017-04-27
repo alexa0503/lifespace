67ab9543b04b5a2e3acac68c3cea488dfc3ac76d
alexa0503 <lori.w@live.cn>
2017-04-27 16:09:45 +0800

    通用版 删除不必要文件

--- resources/views/frame.blade.php
+++
@@ -1,6 +0,0 @@
-<!--外框开始-->
-<div class="Frames-top" style="position:absolute;width: 100%; height: 25px; left: 0px; top: -3px;  background-image: url('{{asset("assets/img/frame/Frames_top.png")}}');"></div>
-<div class="Frames-bottom{{Request::segment(2) == 'index' ? ' Frames-bottom-home' : ''}}" style="position:absolute;width: 100%; height: 25px; left: 0px; bottom: 0px;  background-image: url('{{asset("assets/img/frame/Frames_bottom.png")}}');"></div>
-<div class="Frames-left" style="position:absolute;width: 25px; height: 100%; left: 0px; top: -4px;  background-image: url('{{asset("assets/img/frame/Frames_left.png")}}');"></div>
-<div class="Frames-right" style="position:absolute;width: 25px; height: 100%; right: 0px; top: -4px;  background-image: url('{{asset("assets/img/frame/Frames_right.png")}}');"></div>
-<!--外框结束-->
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,27 +8,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="apple-touch-icon" href="apple-touch-icon.png">
 
-	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/main.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/materialize.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/bgcolor.css')}}">
-    <!--[if IE 9]><link rel="stylesheet" type="text/css" href="http://www.taylormadegolf.cn/css/ie9.css"><![endif]-->
-    <!--[if lt IE 9]><link rel="stylesheet" type="text/css" href="http://www.taylormadegolf.cn/css/ie.css"><![endif]-->
-    @yield('styles')
-
 	</head>
-    <body @yield('bodyStyle') data-img-root-dir="{{asset('assets')}}/" >
+    <body>
         @yield('body')
-
-        <script src="{{asset('assets/js/jquery1.9.1.min.js')}}"></script>
-        <script src="{{asset('assets/js/jquery.touchSwipe.min.js')}}"></script>
-        <script src="{{asset('assets/js/Velocityjs.js')}}"></script>
-        <script src="{{asset('assets/js/oldmain.js')}}"></script>
-        <script src="{{asset('assets/script/main.js')}}"></script>
         @yield('scripts')
-		<div style="display:none;">
-			<script src="https://s11.cnzz.com/z_stat.php?id=1261464287&web_id=1261464287" language="JavaScript"></script>
-		</div>
     </body>
 </html>
