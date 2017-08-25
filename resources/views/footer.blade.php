9e15b87679150d79db3caf4487ac77812e501bdc
小A <wanga503@outlook.com>
2017-08-25 14:34:08 +0800

    底部备案链接

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="sub-main footer subpagefooter">
     <div class="macji" id="ori-footer">
         <ul class="macji-skin">
-            <li class="footer-copyright" style="border-left:none;">© 2017 EVOLUTION HEALTH沪ICP备15030014号-2</li>
+            <li class="footer-copyright" style="border-left:none;"><a href="http://www.miitbeian.gov.cn">© 2017 EVOLUTION HEALTH沪ICP备15030014号-2</a></li>
 
             <li class="footer-first footer-link"><a href="http://www.healthy-lifespace.com/privacy/">法律声明</a></li>
             <li class="footer-link"><a href="http://www.healthy-lifespace.com/" target="_blank">访问澳洲网页<img style="margin-left:5px;" src="{{asset('assets/img/copyright_AustraliaFlag.png')}}"/></a></li>
